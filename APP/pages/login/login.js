9d53932c90eadef4363f54af24406753c2ba0e80
kiffa <editionstyle@live.com>
2018-11-04 15:13:04 +0800

    注册界面，登录界面

--- APP/pages/login/login.js
+++ APP/pages/login/login.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    Email:'',
+    UserName:'',
     Password:'',
 
   },
@@ -22,13 +22,59 @@ Page({
       url: '/pages/register/register',
     })
   },
+  InputUserName: function (e) {
+    this.setData({
+      UserName: e.detail.value
+    })
+  },
+  InputPassword:function(e){
+    this.setData({
+      Password:e.detail.value
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
 
   },
-
+  submit: function (e) {
+    var that = this
+    if (this.data.UserName == '') {
+      wx.showToast({
+        title: '请输入用户名',
+      })
+    } else if (this.data.Password == '') {
+      wx.showToast({
+        title: '请输入密码'
+      })
+    }  else {
+      wx.request({
+        url: ' http://johnnyzen.cn:8080/forget-words-notebook/login/api ',
+        data: {
+          username: this.data.UserName,
+          password: this.data.Password,
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res.data);
+          wx.redirectTo({
+            url: '',//跳转至用户首页
+          })
+        },
+        fail: function (err) {
+          console.log(err);
+          wx.showToast({
+            title: '用户名或密码错误',
+          })
+        },
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面显示
    */
--- APP/pages/register/register.js
+++ APP/pages/register/register.js
@@ -6,6 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    NewUserName: '',
     NewEmail: '',
     NewPassword: '',
     NewPasswordConfirm: '',
@@ -15,13 +16,18 @@ Page({
 
   return_home: function(e) {
     wx.navigateTo({
-      url: '/pages/login/loginPage/loginPage',
+      url: '/pages/login/login',
     })
 
   },
+  inputNewUserName: function(e) {
+    this.setData({
+      NewUserName: e.detail.value
+    })
+  },
   inputNewEmail: function(e) {
     this.setData({
-      Email: e.detail.value
+      NewEmail: e.detail.value
     })
   },
   inputNewPassword: function(e) {
@@ -38,31 +44,48 @@ Page({
   },
   submit: function(e) {
     var that = this
-    if (this.data.NewEmail == ' ') {
+    if (this.data.NewUserName == '') {
       wx.showToast({
-        title: '请输入邮箱地址',
+        title: '请输入用户名',
       })
     } else if (this.data.NewPassword == '') {
       wx.showToast({
         title: '请输入密码'
       })
-      //return
     } else if (this.data.NewPasswordConfirm != this.data.NewPassword) {
       wx.showToast({
         title: '两次密码不一致'
       })
-      return
-    }// else {
-      //success: function(res) {
-        //wx.showToast({
-          //title: '提交成功~'
-        //})
-       // console.log(res)
-       // that.setData({
-         // success: true
-       // })
-      //}
-   // }
+    } else if (this.data.NewEmail == '') {
+      wx.showToast({
+        title: '请输入邮箱'
+      })
+    } else {
+        wx.request({
+          url: ' http://johnnyzen.cn:8080/forget-words-notebook/register/api ',
+          data:{
+            username:this.data.NewUserName,
+            password:this.data.NewPassword,
+            email:this.data.NewEmail,
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          method: 'POST',
+          success:function(res){
+            console.log(res.data);
+            wx.redirectTo({
+              url: '/pages/registerApplyResult/registerSuccess/registerSuccess',
+            })
+          },
+          fail:function(err){
+            console.log(err);
+            wx.redirectTo({
+              url: '/pages/registerApplyResult/registerFailed/registerFailed',
+            })
+          },
+        })
+    }
   },
   /**
    * 用户点击右上角分享
