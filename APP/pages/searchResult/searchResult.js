aed8ad3e6cf82fff0929942dccdd573c6b8c7d17
13730665779 <39216982+13730665779@users.noreply.github.com>
2018-11-05 21:56:24 +0800

    Update searchResult.js
    searchResult Background implementation

--- APP/pages/searchResult/searchResult.js
+++ APP/pages/searchResult/searchResult.js
@@ -5,116 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
-    words: [
-      {
-        word: "family",
-        pronunciation: "/'famely/",
-        meaning: "n.家庭",
-        state: true
-      },
-      {
-        word: "fame",
-        pronunciation: "/'fa:me/",
-        meaning: "n.声望",
-        state: false
-      }
-    ]
+    words: []
   },
-  cancel: function(e){
+  cancel: function(e) {
     wx.navigateTo({
       url: '../index/index',
     })
   },
-  wordTo: function(e){
+  wordTo: function(e) {
     wx.navigateTo({
       url: '../word/word',
     })
   },
-  searchInput: function(e){
-    this.setData({
-      words: [{
-        word: e.detail.value
-      }]
-    });
+  searchInput: function(e) {
+    // this.setData({
+    //   words: [{
+    //     word: e.detail.value
+    //   }]
+    // });
     var that = this;
-    console.log(e.detail.value);
     wx.request({
-      url: '',//此处填写你后台请求地址
-      method: 'POST',// OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      header: { 'Content-Type': 'application/json' },// 设置请求的 header
-
-      success: function (res) {
+      url: 'http://johnnyzen.cn:8080/forget-words-notebook/searchWords/api',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'JSESSIONID=' + wx.getStorageSync('wxSession')
+      },
+      method: 'POST',
+      data: {
+        search: e.detail.value,
+        token: wx.getStorageSync('token')
+      },
+      success: function(res) {
         // success
-        console.log(res.data);//在控制台输出向服务器接收到的数据
-        that.setData({ //======不能直接写this.setDate======
-          words: res.data //在相应的wxml页面显示接收到的数据
+        that.setData({
+          words: [
+            res.data.data.content[0]
+          ]
         });
       },
-      fail: function (res) {
-        // fail
-      },
-      complete: function (res) {
-        // complete
+      fail: function(res) {
+        console.log(res.data); //在控制台输出向服务器接收到的数据
       }
     })
 
+
   },
-  addWord: function (e) {
+  addWord: function(e) {
     var word = e.target.dataset.word;
     console.log(word);
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
+var Util = require("../../utils/util.js");
