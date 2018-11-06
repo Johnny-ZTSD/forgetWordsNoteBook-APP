139f20b679a288c8afd6aa25d4bc9db6c4b8c7eb
13730665779 <39216982+13730665779@users.noreply.github.com>
2018-11-06 20:18:33 +0800

    Update index.js

--- APP/pages/index/index.js
+++ APP/pages/index/index.js
@@ -4,10 +4,10 @@ const app = getApp()
 
 Page({
   data: {
-    headImg: "../../images/headImg.png",
-    lave: 20,
-    new:43,
-    hightForget: 89
+    headImg: '../../images/headImg.png',
+    lave: 0,
+    everyNewWords: 0,
+    oftenForgotWords: 0
 
   },
   searchWord: function(e) {
@@ -16,45 +16,75 @@ Page({
     })
   },
   recite: function(e) {
+    //设置状态为背任务单词
+    app.globalData.state = 'recite';
     wx.navigateTo({
       url: '../reciteword/reciteword',
     })
   },
   casual: function(e) {
+    //设置状态为随便背背
+    app.globalData.state = 'casual';
     wx.navigateTo({
       url: '../reciteword/reciteword',
     })
   },
 
   //事件处理函数
-  binMyPage: function() {
-      wx.navigateTo({
-          url: '../myPage/myPage'
-      })
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
   onLoad: function (options) {
-    var that = this;//=====注意此处，要用that 指代this=====
+   
+  },
+  onShow: function() {
+    console.log(this.data.headImg);
+    var that = this;
+    //请求每日生词数据
     wx.request({
-      url: '',//此处填写你后台请求地址
-      method: 'GET',// OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      header: { 'Content-Type': 'application/json' },// 设置请求的 header
-
+      url: 'http://johnnyzen.cn:8080/forget-words-notebook/viewEverydayNewWords/api',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'JSESSIONID=' + wx.getStorageSync('wxSession')
+      },
+      method: 'POST',
+      data: {
+        token: wx.getStorageSync('token')
+      },
       success: function (res) {
         // success
-        console.log(res.data);//在控制台输出向服务器接收到的数据
-        var headImg = res.data.headImg;
-        that.setData({ //======不能直接写this.setDate======
-          headImg: headImg //在相应的wxml页面显示接收到的数据
+        that.setData({
+          everyNewWords: res.data.data.content.length
         });
       },
       fail: function (res) {
-        // fail
-      },
-      complete: function (res) {
-        // complete
+        console.log(res.data); //在控制台输出向服务器接收到的数据
       }
     })
-
+    //请求高频忘词数据
+    wx.request({
+      url: 'http://johnnyzen.cn:8080/forget-words-notebook/viewOftenForgotWords/api',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': 'JSESSIONID=' + wx.getStorageSync('wxSession')
+      },
+      method: 'POST',
+      data: {
+        token: wx.getStorageSync('token')
+      },
+      success: function (res) {
+        // success
+        that.setData({
+          oftenForgotWords: res.data.data.content.length
+        });
+        console.log(res.data.data.content);
+      },
+      fail: function (res) {
+        console.log(res.data); //在控制台输出向服务器接收到的数据
+      }
+    })
   },
 
   getUserInfo: function(e) {
