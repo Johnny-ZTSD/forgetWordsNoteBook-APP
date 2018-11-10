b3f7d7e92c6bd682516167d02f32aadad77d38c8
Tingting@Wang <39662863+Wangtingting152@users.noreply.github.com>
2018-11-10 19:25:58 +0800

    Update resetPassword.js

--- APP/pages/resetPassword/resetPassword.js
+++ APP/pages/resetPassword/resetPassword.js
@@ -1,18 +1,32 @@
 // pages/resetPassword/resetPassword.js
+const app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+
   },
-  savePassword:function(){
-    wx.navigateTo({
-      url: '../userInfor/userInfor',
-    })
+  savePassword: function(e) {
+    var formData = e.detail.value;
+    formData.openid = 11;
+    wx.request({
+        url: 'http://johnnyzen.cn:8080/forget-words-notebook/resetPassword/api ',
+        data: formData,
+        method: 'POST',
+        header: { // 设置请求的 header
+          'content-type': 'application/x-www-form-urlencoded',
+          'Cookie': 'JSESSIONID=' + wx.getStorageSync('wxSession')
+        },
+        success: function(res) {
+          console.log(JSON.stringify(res.data))
+        },
+              fail: function(res) {
+          console.log('错误' + ':' + res)
+        }
+      }),
+      wx.navigateTo({
+        url: '../userInfor/userInfor',
+      })
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.setNavigationBarTitle({
       title: "重置密码" //页面标题修改为个人信息
     })
