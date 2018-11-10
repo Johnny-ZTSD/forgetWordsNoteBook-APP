82246e98459aaaf32e377c9ec7c615cd2efa89e2
Tingting@Wang <39662863+Wangtingting152@users.noreply.github.com>
2018-11-10 19:00:48 +0800

    Update myPage.js

--- APP/pages/myPage/myPage.js
+++ APP/pages/myPage/myPage.js
@@ -1,35 +1,40 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
-    userInfo: { avatarUrl: "../../../images/headImg.png", nickName: "千千寰宇", sex: "../../../images/sex_boy.png", email: "11223344@163.com" },
-    showView: false,
-    // canIUse: wx.canIUse('button.open-type.getUserInfo')
+    name: 'wcb',
+    email: '11102846719@163.com',
+    sex: 'U',
+    avatarUrl: "../../../images/headImg.png",
+    sexImg: ["../../../images/sex_boy.png", "../../../images/sex_boy.png"],
+    token:"",
+    
   },
   //事件处理函数
-  bindUserInfor: function () {
+  bindUserInfor: function() {
     wx.navigateTo({
       url: '../userInfor/userInfor',
     })
   },
-  BinQuitLogin:function(){
-    wx.navigateTo({
-      url:'', //登录页面的地址
+  dropOut: function() {
+    wx.request({
+      url: 'http://johnnyzen.cn:8080/forget-words-notebook/exitLogin/api ', //退出登录API
+    })
+    wx.switchTab({
+      url: '../index/index', //登录页面的地址
     })
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
+
     // 生命周期函数--监听页面加载
-    showView: (options.showView == "true" ? true : false)
-  },
-  onChangeShowState: function () {
-    var that = this;
-    that.setData({
-      showView: (!that.data.showView)
+    this.setData({
+      name: app.globalData.userInfo.username,
+      email: app.globalData.userInfo.email,
+      sex: app.globalData.userInfo.sex,
     })
   },
-  getUserInfo: function (e) {
-    
+  onShow:function(){
+    console.log("个人中心" + this.data)
   }
 })
