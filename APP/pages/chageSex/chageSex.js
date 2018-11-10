44a67f621f3c40b500383e11bbaaae8013cf9699
Tingting@Wang <39662863+Wangtingting152@users.noreply.github.com>
2018-11-10 19:18:11 +0800

    Update chageSex.js

--- APP/pages/chageSex/chageSex.js
+++ APP/pages/chageSex/chageSex.js
@@ -1,23 +1,56 @@
-// pages/chageSex/chageSex.js
+var adds = {};
 Page({
-    
   data: {
-    items: [
-      { name: 'boy', value: '男', checked: 'true' },
-      { name: 'girl', value: '女' },
-    ]
+    img_arr: [],
+    formdata: '',
   },
-  radioChange:function(e){
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
-  },
-  binSaveSex:function(){
-    wx.navigateTo({
-      url: '../userInfor/userInfor',
-    })
-  },
-  onLoad: function (options) {
-  
+  formSubmit: function(e) {
+    var id = e.target.id
+    adds = e.detail.value;
+    adds.program_id = app.jtappid
+    adds.openid = app._openid
+    adds.zx_info_id = this.data.zx_info_id
+    this.upload()
   },
 
-  
+  upload: function() {
+    var that = this
+    wx.uploadFile({
+        url: 'https:***/submit',
+        filePath: that.data.img_arr[0],
+        name: 'content',
+        formData: adds,
+        success: function(res) {
+          console.log(res)
+          if (res) {
+            wx.showToast({
+              title: '已提交发布！',
+              duration: 3000
+            });
+          }
+        }
+      }),
+      this.setData({
+        formdata: ''
+      })
+  },
+  upimg: function() {
+    var that = this;
+    if (this.data.img_arr.length < 3) {
+      wx.chooseImage({
+        sizeType: ['original', 'compressed'],
+        success: function(res) {
+          that.setData({
+            img_arr: that.data.img_arr.concat(res.tempFilePaths)
+          })
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '最多上传三张图片',
+        icon: 'loading',
+        duration: 3000
+      });
+    }
+  }
 })
